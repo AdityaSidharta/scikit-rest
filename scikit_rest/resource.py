96c3ec4933a303014988424006e9d698ee4c71fd
adityasidharta <aditya@ucare.io>
2019-04-28 16:36:07 +0800

    Added capabilities on col_types - specifying allowed values. allow sklearn.model to be passed in predict_fn

--- scikit_rest/resource.py
+++ scikit_rest/resource.py
@@ -1,30 +1,33 @@
 import numpy as np
 import pandas as pd
-import datetime
-from dateutil.parser import parse
 from collections import OrderedDict
 from flask_restful import Resource, reqparse
+import sklearn
 
 from scikit_rest.validator import validate_args
-from scikit_rest.type import set_type
+from scikit_rest.types import set_type
+
+from typing import List, Dict, Union, Any, Callable
 
 
 class Prediction(Resource):
-    def __init__(self, **kwargs):
-        self.col_list = kwargs["col_list"]
-        self.col_types = kwargs["col_types"]
-        self.transform_fn = kwargs["transform_fn"]
-        self.predict_fn = kwargs["predict_fn"]
-        self.is_nullable = kwargs["is_nullable"]
+    """class to handle ML prediction"""
 
-        self.parser = reqparse.RequestParser()
+    def __init__(self, **kwargs):
+        self.col_list: List[str] = kwargs["col_list"]
+        self.col_types: Dict[str, Union[List, type]] = kwargs["col_types"]
+        self.transform_fn: Callable = kwargs["transform_fn"]
+        self.predict_fn: Union[Callable, sklearn.base.BaseEstimator] = kwargs["predict_fn"]
+        self.is_nullable: bool = kwargs["is_nullable"]
+
+        self.parser: reqparse.RequestParser = reqparse.RequestParser()
         for col in self.col_list:
             self.parser.add_argument(col)
-        self.args = self.parser.parse_args()
+        self.args: Dict[str, Any] = self.parser.parse_args()
 
-        self.result = np.nan
-        self.status_message = "Success"
-        self.status_code = 200
+        self.result: Any = np.nan
+        self.status_message: str = "Success"
+        self.status_code: int = 200
 
         is_success, status_message = validate_args(self.args, self.col_types, self.is_nullable)
         if not is_success:
@@ -45,5 +48,20 @@ class Prediction(Resource):
                 input_dict[col] = [value]
             input_df = pd.DataFrame(input_dict)
             transformed_df = self.transform_fn(input_df)
-            self.result = self.predict_fn(transformed_df)
+            if isinstance(self.predict_fn, sklearn.base.BaseEstimator):
+                if hasattr(self.predict_fn, "predict_proba"):
+                    if len(transformed_df) == 1:
+                        self.result = self.predict_fn.predict_proba(transformed_df)[:, 1].item()
+                    else:
+                        self.result = self.predict_fn.predict_proba(transformed_df)[:, 1]
+                elif hasattr(self.predict_fn, "predict"):
+                    if len(transformed_df) == 1:
+                        self.result = self.predict_fn.predict(transformed_df).item()
+                    else:
+                        self.result = self.predict_fn.predict(transformed_df)
+                else:
+                    self.status_code = 401
+                    self.status_message = "predict_fn does not have attributes predict or predict_proba"
+            else:
+                self.result = self.predict_fn(transformed_df)
         return self.output()
--- scikit_rest/validator.py
+++ scikit_rest/validator.py
@@ -1,18 +1,31 @@
 import datetime
 import pandas as pd
 
-from scikit_rest.type import is_date, is_bool, is_float, is_int, is_str
+from scikit_rest.types import validate_type
+
+from typing import Dict, Union, List
 
 
-def validate_type(col_types):
+def validate_col_types(col_types: Dict[str, Union[List, type]]):
+    """assert that all the values within the dictionary is one of the following : [str, int, float, bool, datetime]"""
     for key, items in col_types.items():
-        assert items in [str, int, float, bool, datetime.datetime], (
-            "Key {} has an unapproved type {}. Type must be one of the following : "
-            "[str, int, float, bool, datetime.datetime ]".format(key, items)
-        )
+        if isinstance(items, type):
+            assert items in [str, int, float, bool, datetime.datetime], (
+                "Key {} has an unapproved type {}. Type must be one of the following : "
+                "[str, int, float, bool, datetime.datetime]".format(key, items)
+            )
+        else:
+            assert isinstance(items, list), "values of key {} should be a list if it is not a type".format(key)
+            for item in items:
+                assert isinstance(item, str), "if col_types {} is a list, values must be type str".format(key)
 
 
-def validate_args(args, col_types, is_nullable):
+def validate_args(
+    args: Dict[str, Union[str, bool, int, float, datetime.datetime]],
+    col_types: Dict[str, Union[List, type]],
+    is_nullable: bool,
+) -> [bool, Dict[str, str]]:
+    """check whether the given arguments follows the specified types and follows the nullable rules"""
     status_message = dict()
     for col, col_type in col_types.items():
         if col not in args.keys():
@@ -21,21 +34,11 @@ def validate_args(args, col_types, is_nullable):
             if (not is_nullable) or (col not in is_nullable):
                 status_message[col] = "input {} is not supposed to be null".format(col)
         else:
-            if col_type == datetime.datetime:
-                if not is_date(args[col]):
-                    status_message[col] = "input {} has wrong format (supposed to be {})".format(col, col_type)
-            elif col_type == float:
-                if not is_float(args[col]):
-                    status_message[col] = "input {} has wrong format (supposed to be {})".format(col, col_type)
-            elif col_type == int:
-                if not is_int(args[col]):
-                    status_message[col] = "input {} has wrong format (supposed to be {})".format(col, col_type)
-            elif col_type == bool:
-                if not is_bool(args[col]):
-                    status_message[col] = "input {} has wrong format (supposed to be {})".format(col, col_type)
+            if isinstance(col_type, list):
+                if not validate_type(args[col], col_type):
+                    status_message[col] = "input {} has to be one of the following {}".format(col, col_type)
             else:
-                if not is_str(args[col]):
+                if not validate_type(args[col], col_type):
                     status_message[col] = "input {} has wrong format (supposed to be {})".format(col, col_type)
-
     is_success = len(status_message) == 0
     return is_success, status_message
